612a797fc6746e2d4455f6c1fd869d2b0bf53558
Martin Pitt <martin@piware.de>
2022-09-26 11:55:49 +0200

    appservice: Replace websocat cockpit-ws session script with Python program
    This eliminates the last websocat usage, as it is not packaged anywhere,
    and downloading straight from GitHub isn't appropriate for production.
    
    Fixes #42

--- appservice/scripts/cockpit-ws-session.sh
+++ appservice/scripts/cockpit-ws-session.sh
@@ -2,11 +2,8 @@
 # cockpit session container entry point
 set -eux
 
-echo 'exec websocat -b -s 0.0.0.0:8080' > /tmp/socat-session.sh
-chmod a+x /tmp/socat-session.sh
-
 # we cannot write to /etc as unprivileged user on k8s
 mkdir -p /tmp/conf/cockpit
 printf "[Webservice]\nUrlRoot=${ROUTE_WSS}/sessions/${SESSION_ID}/web\nOrigins = ${API_URL}\n" > /tmp/conf/cockpit/cockpit.conf
 export XDG_CONFIG_DIRS=/tmp/conf
-exec /usr/libexec/cockpit-ws --for-tls-proxy --local-session=/tmp/socat-session.sh
+exec /usr/libexec/cockpit-ws --for-tls-proxy --local-session=/websocket-session.py
---
+++ appservice/scripts/websocket-session.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+import asyncio
+import logging
+import os
+import sys
+
+import websockets
+
+logger = logging.getLogger(__name__)
+
+
+async def ws2out(ws):
+    try:
+        async for message in ws:
+            logger.debug('ws -> stdout: %s', message)
+            os.write(1, message)
+    except websockets.exceptions.ConnectionClosedError as e:
+        logger.debug('ws2out: websocket connection got closed: %s', e)
+        return
+
+
+async def in2ws(reader, ws):
+    while True:
+        message = await reader.read(4096)
+        if not message:
+            logger.debug('in -> ws: EOF')
+            await ws.close()
+            break
+        logger.debug('in -> ws: %s', message)
+        await ws.send(message)
+
+
+async def handler(ws):
+    # wrap stdin in an asyncio stream
+    loop = asyncio.get_event_loop()
+    reader = asyncio.StreamReader(loop=loop)
+    protocol = asyncio.StreamReaderProtocol(reader)
+    await loop.connect_read_pipe(lambda: protocol, sys.stdin.buffer)
+
+    ws2out_task = asyncio.create_task(ws2out(ws))
+    in2ws_task = asyncio.create_task(in2ws(reader, ws))
+    _done, pending = await asyncio.wait([ws2out_task, in2ws_task],
+                                        return_when=asyncio.FIRST_COMPLETED)
+    for task in pending:
+        task.cancel()
+
+
+async def main():
+    async with websockets.serve(handler, '', 8080):
+        await asyncio.Future()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    asyncio.run(main())
